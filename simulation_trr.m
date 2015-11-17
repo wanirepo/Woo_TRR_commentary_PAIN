6fd08a6ad22e22638468f6353eae23223d000656
wanirepo <clinpsywoo@gmail.com>
2015-11-16 20:13:57 -0700

    documentation

--- simulation_trr.m
+++ simulation_trr.m
@@ -1,17 +1,30 @@
-%% one communicative culture
+%% SIMULATION IN Woo & Wager (2015) What reliability can and cannot tell us 
+% about pain report and pain neuroimaging. PAIN. Figure 1
 
-fprintf('iteration 0000');
+%% DEPENDENCIES:
+% ICC.m from http://www.mathworks.com/matlabcentral/fileexchange/21501-intraclass-correlation-coefficients/content/ICC.m
+% violinplot.m from CanlabCore (https://github.com/canlab/CanlabCore)
+
+% NOTE THAT each time you run the code, you will get different figures 
+% becuase the simulation results depend on randomly generated values. 
+
+% Also run this code in the directory where you want to save the figures.
+% This will save figures in the directory you're currently in.
+
+%% SUBJECTS FROM ONE HOMOGENEOUS COMMUNICATIVE CULTURE
+
+fprintf('SIMULATION FOR A HOMOGENEOUS GROUP, ITERATION 0000');
 for iter = 1:1000
     
     fprintf('\b\b\b\b%04d', iter);
     
-    btw_var = 5;
+    btw_var = 5; % between-person variability
     x1 = normrnd(50, btw_var, 30, 1);
     
-    within_var = 5;
+    within_var = 5; % within-person variability
     x2 = x1 + normrnd(0, within_var, 30, 1);
     
-    icc1(iter,1) = ICC(2,'single', [x1 x2]);
+    icc1(iter,1) = ICC(2,'single', [x1 x2]); % ICC 
     
     if icc1(iter,1) > 0.65 && icc1(iter,1) < 0.66
         x11 = x1;
@@ -19,11 +32,21 @@ for iter = 1:1000
     end
 end
 
-disp('done');
+% Get the representative data (values close to mean icc)
+icc_rep = 0;
+while icc_rep < floor(mean(icc1)*100)/100 || icc_rep > ceil(mean(icc1)*100)/100
+    x11 = normrnd(50, btw_var, 30, 1);
+    x22 = x11 + normrnd(0, within_var, 30, 1);
+    
+    icc_rep = ICC(2,'single', [x11 x22]); % ICC 
+end    
 
-%% two communicative cultures: stoic vs. communicators
+disp(' DONE.');
 
-fprintf('iteration 0000');
+%% SUBJECTS FROM TWO HETEROGENEUOUS COMMUNICATIVE CULTURES: 
+% STOIC VS. COMMUNICATORS
+
+fprintf('SIMULATION FOR HETEROGENEOUS GROUPS, ITERATION 0000');
 for iter = 1:1000
     
     fprintf('\b\b\b\b%04d', iter);
@@ -40,16 +63,23 @@ for iter = 1:1000
     x4 = x3 + normrnd(0, within_var, 30, 1);
     
     icc2(iter,1) = ICC(2,'single', [x3 x4]);
-    
-    if icc2(iter,1) > 0.79 && icc2(iter,1) < 0.80
-        x33 = x3;
-        x44 = x4;
-    end
+
 end
 
-disp('done');
+% Get the representative data (values close to mean icc)
+icc_rep = 0;
+while icc_rep < floor(mean(icc2)*100)/100 || icc_rep > ceil(mean(icc2)*100)/100
+    comm = normrnd(55, btw_var, 15, 1);
+    stoic = normrnd(45, btw_var, 15, 1);
+    x33 = [comm; stoic];
+    x44 = x33 + normrnd(0, within_var, 30, 1);
+    
+    icc_rep = ICC(2,'single', [x33 x44]); % ICC 
+end    
 
-%% plots: 1 GROUP
+disp(' DONE.');
+
+%% PLOTS: HOMOGENEOUS GROUPS
 close all;
 
 figure('color', 'w');
@@ -72,8 +102,7 @@ set(gca, 'fontsize', 20, 'xlim', [0.7 2.2], 'ylim', [20 80], ...
     'xtick', [1 2], 'xticklabel', [], 'tickDir', 'out', 'tickLength', [.02 .02], 'linewidth', 1.5);
 set(gcf, 'position', [360   483   176   215]);
 
-savedir = '/Users/clinpsywoo/Dropbox/W_Letzen_commentary';
-savename = fullfile(savedir, 'sim_1group.pdf');
+savename = 'sim_1group.pdf';
 
 try
     pagesetup(gcf);
@@ -83,30 +112,7 @@ catch
     saveas(gcf, savename);   
 end
 
-%% 2 GROUPS
-% 
-% close all;
-% 
-% figure('color', 'w');
-% col = cbrewer('seq', 'YlOrRd', 30); col = flipud(col);
-% edgecol = col*.7;
-% 
-% r = normrnd(0,.05, 30,1);
-% for i = 1:numel(x3)
-%     scatter(1+r(i), x3(i), 80, col(i,:), 'filled')
-%     hold on;
-% end
-% 
-% hold on;
-% for i = 1:numel(x11)
-%     line([1+r(i), 2+r(i)], [x3(i), x4(i)], 'color', [.6 .6 .6]);
-% end
-% 
-% for i = 1:numel(x3)
-%     scatter(1+r(i), x3(i), 80, col(i,:), 'filled')
-%     hold on;
-%     scatter(2+r(i), x4(i), 80, col(i,:), 'filled')
-% end
+%% PLOTS: HETEROGENEOUS  GROUPS
 
 close all;
 
@@ -131,8 +137,7 @@ set(gca, 'fontsize', 20, 'xlim', [0.7 2.2], 'ylim', [20 80], ...
     'xtick', [1 2], 'xticklabel', [], 'tickDir', 'out', 'tickLength', [.02 .02], 'linewidth', 1.5);
 set(gcf, 'position', [360   483   176   215]);
 
-savedir = '/Users/clinpsywoo/Dropbox/W_Letzen_commentary';
-savename = fullfile(savedir, 'sim_2group.pdf');
+savename = 'sim_2group.pdf';
 
 try
     pagesetup(gcf);
@@ -142,12 +147,11 @@ catch
     saveas(gcf, savename);   
 end
 
-save(fullfile(savedir, 'sim_res.mat'), 'x*', 'icc*');
+save('sim_res.mat', 'x*', 'icc*');
 
-%% ICC for group 1 (run this on matlab 2015)
+%% ICC VIOLIN PLOTS
 
-savedir = '/Users/clinpsywoo/Dropbox/W_Letzen_commentary';
-load(fullfile(savedir, 'sim_res.mat'));
+load('sim_res.mat');
 
 col = [0.4392    0.6784    0.2784
     0.9294    0.4902    0.1922];
@@ -156,21 +160,18 @@ edgecol = col*.7;
 close all;
 create_figure('violin');
 violinplot([icc1 icc2], 'facecolor',col, 'edgecolor', edgecol, 'facealpha', 1, ...
-    'mc', 'k', 'medc', '', 'plotlegend', 0, 'nopoints'); 
-%set(gcf, 'position', [360   416   350   282], 'color', 'w'); 
+    'mc', 'k', 'medc', '', 'plotlegend', 0, 'nopoints', 'linewidth', 2); 
 set(gcf, 'position', [360   483   234   217]);
 set(gca, 'FontSize', 20, 'linewidth', 1.5, 'box', 'off', 'ylim', [0 1],...
     'TickDir', 'out', 'TickLength', [.02 .02], 'xticklabel', [], 'ytick', 0:.2:1);
 
-%%
-savedir = '/Users/clinpsywoo/Dropbox/W_Letzen_commentary';
-savename = fullfile(savedir, 'sim_violin.pdf');
+savename = 'sim_violin.pdf';
 
 try
-pagesetup(gcf);
-saveas(gcf, savename);
+    pagesetup(gcf);
+    saveas(gcf, savename);
 catch
-pagesetup(gcf);
-saveas(gcf, savename);
+    pagesetup(gcf);
+    saveas(gcf, savename);
 end
 
